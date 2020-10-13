d20666f9d553bc4e0932a84f0c0300cae8ff0b1a
baju0218 <baju0218@naver.com>
2020-10-13 23:40:43 +0900

    Done

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import render as ren
 import numpy as np
 
 sim.loadEnvironment("data\plane.obj", thickness = 0.001, penetrationCoef = 1., elasticityCoef = 0., frictionResistanceCoef = 50., frictionCoef = 10., scale = 100., basePosition = np.array([0., 0., 0.]), baseOrientation = np.array([0., 0., 0.]))
-sim.loadSnake("data\snake.veg", timestep = 0.01, dampingMassCoef = 0., dampingStiffnessCoef = 0.01, gravity = 10., muscleStiffnessCoef = 10000., basePosition = np.array([0., 0.2, 0.]), baseOrientation = np.array([0., 0., 0.]))
+sim.loadSnake("data\snake.veg", timestep = 0.01, dampingMassCoef = 0., dampingStiffnessCoef = 0.01, gravity = 10., muscleStiffnessCoef = 10000., basePosition = np.array([0., 0.1, 0.]), baseOrientation = np.array([0., 0., 0.]))
 sim.setGoal(1., 0., 1.)
 
 ren.createWindow(1080, 1080, "[Test] Snake Locomotion")
